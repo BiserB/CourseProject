c0560f1809b960f666f07437e3a1cbaa5a4c7c66
BiserBorisov <butoff@abv.bg>
2018-08-31 12:04:08 +0300

    Voted in poll redirection

--- Vote.App/Areas/Participant/Controllers/PollsController.cs
+++ Vote.App/Areas/Participant/Controllers/PollsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using Vote.App.Infrastructure.Session;
 using Vote.Common;
 using Vote.Common.BindingModels;
+using Vote.Common.ViewModels.Polls;
 using Vote.Services.Participant.Interfaces;
 
 namespace Vote.App.Areas.Participant.Controllers
@@ -16,21 +18,21 @@ namespace Vote.App.Areas.Participant.Controllers
         }
 
         [HttpGet]
-        public IActionResult Vote(int id)
+        public IActionResult Vote(int id, string code)
         {
-            var votedInPoll = this.HttpContext.Session.VotedInPoll(id.ToString());
-
-            if (votedInPoll)
-            {
-                return RedirectToAction("PollResults", new { id });
-            }
-
             var pollModel = this.service.GetActivePoll(id);
 
             if (pollModel == null)
             {
-                return LocalRedirect("~/NoResult");
+                return RedirectToAction("PollResults", new { id = 0, code});
             }
+
+            var votedInPoll = this.HttpContext.Session.VotedInPoll(pollModel.PollId.ToString());
+
+            if (votedInPoll)
+            {
+                return RedirectToAction("PollResults", new { id = pollModel.PollId, code});
+            }            
 
             return this.View(pollModel);
         }
@@ -48,15 +50,22 @@ namespace Vote.App.Areas.Participant.Controllers
 
                     this.HttpContext.Session.SetPollId(pollId, VoteConstants.Voted);
                 }
-            }
+            }            
 
-            return RedirectToAction("PollResults", new { id = model.PollId });
+            return RedirectToAction("PollResults", new { id = model.PollId, code = model.EventCode });
         }
 
         [HttpGet]
-        public IActionResult PollResults(int id)
+        public IActionResult PollResults(int id, string code)
         {
-            var pollResultsModel = this.service.GetPollResults(id);
+            ViewData["BackUrl"] = code;
+
+            if (id == 0)
+            {
+                return this.View(new List<PollResultModel>());
+            }
+            
+            var pollResultsModel = this.service.GetPollResults(id);            
 
             return this.View(pollResultsModel);
         }
